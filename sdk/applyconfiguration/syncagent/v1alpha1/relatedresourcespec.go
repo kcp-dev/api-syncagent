f68f7764fbb29f6e6e67d304dc4242e2eeb77e36
Karol Szwaj <karol.szwaj@gmail.com>
2025-03-26 13:25:20 +0100

    add the unit tests for related resources
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    On-behalf-of: @SAP karol.szwaj@sap.com

--- sdk/applyconfiguration/syncagent/v1alpha1/relatedresourcespec.go
+++ sdk/applyconfiguration/syncagent/v1alpha1/relatedresourcespec.go
@@ -21,11 +21,19 @@ package v1alpha1
 // RelatedResourceSpecApplyConfiguration represents a declarative configuration of the RelatedResourceSpec type for use
 // with apply.
 type RelatedResourceSpecApplyConfiguration struct {
+<<<<<<< HEAD
 	Identifier *string                                  `json:"identifier,omitempty"`
 	Origin     *string                                  `json:"origin,omitempty"`
 	Kind       *string                                  `json:"kind,omitempty"`
 	Object     *RelatedResourceObjectApplyConfiguration `json:"object,omitempty"`
 	Mutation   *ResourceMutationSpecApplyConfiguration  `json:"mutation,omitempty"`
+=======
+	Identifier *string                                     `json:"identifier,omitempty"`
+	Origin     *string                                     `json:"origin,omitempty"`
+	Kind       *string                                     `json:"kind,omitempty"`
+	Reference  *RelatedResourceReferenceApplyConfiguration `json:"reference,omitempty"`
+	Mutation   *ResourceMutationSpecApplyConfiguration     `json:"mutation,omitempty"`
+>>>>>>> 05c1537 (add the unit tests for related resources)
 }
 
 // RelatedResourceSpecApplyConfiguration constructs a declarative configuration of the RelatedResourceSpec type for use with
@@ -71,13 +79,5 @@ func (b *RelatedResourceSpecApplyConfiguration) WithObject(value *RelatedResourc
 // If called multiple times, the Mutation field is set to the value of the last call.
 func (b *RelatedResourceSpecApplyConfiguration) WithMutation(value *ResourceMutationSpecApplyConfiguration) *RelatedResourceSpecApplyConfiguration {
 	b.Mutation = value
-	return b
-}
-
-// WithOptional sets the Optional field in the declarative configuration to the given value
-// and returns the receiver, so that objects can be built by chaining "With" function invocations.
-// If called multiple times, the Optional field is set to the value of the last call.
-func (b *RelatedResourceSpecApplyConfiguration) WithOptional(value bool) *RelatedResourceSpecApplyConfiguration {
-	b.Optional = &value
 	return b
 }
